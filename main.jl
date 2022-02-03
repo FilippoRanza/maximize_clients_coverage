9f9f59464f1570d6002572d2cab23e0b40bb9624
Filippo Ranza <filipporanza@gmail.com>
2022-02-03 14:19:20 +0100

    Add new constraints and objective function

--- main.jl
+++ main.jl
@@ -7,23 +7,27 @@ using Cbc
 model = Model(Cbc.Optimizer)
 set_optimizer_attribute(model, "threads", 8) 
 
-n = 100;
-m = 10000;
-ϕ = 1000rand(n) .+ 1500;
-δ = rand(0:1, n, m);
-α = rand(n, m) .* δ ;
-λ = 100rand(n);
+n = 100
+m = 10000
+ϕ = 1000rand(n) .+ 1500
+δ = rand(0:1, n, m)
+α = rand(n, m) .* δ
+λ = 100rand(n)
 ν = rand(1:6, n)
+K = rand(50:100)
 
 
+@time @variable(model, 0 <= x[1:n], Int)
+@time @variable(model, z[1:n], Bin)
+@time @variable(model, y[i = 1:n, j = 1:m], Bin)
 
-@variable(model, 0 <= x[1:n], Int)
-@variable(model, z[1:n], Bin)
+@time @constraint(model, c0, 2000sum(z) + 12000sum(x) + sum(ϕ .* x) <= 100000)
+@time @constraint(model, c1,  x .<= ν .* z)
+@time @constraint(model, c3, K .* x .<=  λ )
+ 
+@time c4 = @constraint(model, [i = 1:n], α[i, :]' * y[i, :] <= K .* x[i])
 
-@constraint(model, c0, 2000sum(z) + 15000sum(x) + sum(ϕ .* x) <= 1000000)
-@constraint(model, c1,  x .<= ν .* z)
-
-@objective(model, Max, sum(sum(α, dims=2) .* x))
+@time @objective(model, Max, α[:]' * y[:])
 optimize!(model)
 if termination_status(model) == OPTIMAL
     println(value.(x))
