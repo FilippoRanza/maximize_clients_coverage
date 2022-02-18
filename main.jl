8b37b53c4abc5148d21a417349c73182652c2764
Filippo Ranza <filipporanza@gmail.com>
2022-02-18 15:13:42 +0100

    Add instance builder from clients and stations

--- main.jl
+++ main.jl
@@ -1,9 +1,27 @@
 using Cbc
 using HDF5
 using JuMP
+using YAML
+using Configurations
+
+include("make_instance.jl")
 
 model = Model(Cbc.Optimizer)
 set_optimizer_attribute(model, "threads", 8) 
+
+@option struct Configuration
+    client_file::String
+    client_resource::String
+    station_file::String    
+    station_resource::String
+end
+
+function load_config(file_name)
+    data = YAML.load_file(file_name, dicttype=Dict{String, Any})
+    from_dict(Configuration, data)
+end
+
+
 
 struct ModelVariables 
     x
@@ -27,20 +45,24 @@ function build_model!(model, A, Γ, c, Φ, Π, Σ, κ, t, Λ)
     ModelVariables(x, y, z)
 end
 
+config = load_config("config.yml")
 
-n = 10
-m = 10
+
+clients = h5read(config.client_file, config.client_resource)
+stations = h5read(config.station_file, config.station_resource)
+
+
+A, Λ = make_instance(clients, stations)
+n, m = size(A)
+
 ϕ = 1000rand(n) .+ 1500
-δ = rand(0:1, n, m)
-α = rand(n, m) .* δ
-λ = 100rand(n)
 ν = rand(1:6, n)
 K = rand(50:100)
 
-vars = build_model!(model, α, 1500ones(n), 15000, ϕ, zeros(n), ν, K, 25000, λ)
-# optimize!(model)
+vars = build_model!(model, A, 1500ones(n), 15000, ϕ, zeros(n), ν, K, 25000, Λ)
+optimize!(model)
 if termination_status(model) == OPTIMAL
     println(value.(vars.x))
-    println(value.(vars.y))
+    # println(value.(vars.y))
     println(value.(vars.z))
 end
--- make_instance.jl
+++ make_instance.jl
@@ -1,31 +1,31 @@
 
-
-function make_instance(client_matrix, station_list; threashold=1e-3, scale=250)
-    A, idx = init_output(client_matrix, station_list)
-    scale /= log(2)
-    for (j, (sx, sy)) in enumerate(eachrow(station_list))
-        p = (x, y) -> exp(-dist(x, y, sx, sy) / scale)
-        for (i, (cx, cy, _)) in enumerate(idx) 
-            A[i, j] = p(cx, cy) 
+function make_instance(client_matrix, station_matrix; p_dist=2, threashold=1e-3)
+    A, Λ = init_alpha_matrix(client_matrix, station_matrix)
+    
+    for (j, (x, y, v, d_csr, d_cli)) in enumerate(eachrow(client_matrix))
+        p_csr, p_cli = make_probability(x, y, d_csr, d_cli, p_dist)
+        for (i, (sx, sy)) in enumerate(eachrow(station_matrix))
+            A[i, j] = p_cli(sx, sy) * v 
+            Λ[i] += p_csr(sx, sy) * v
         end
     end
     A[A .<= threashold] .= 0
-    A, idx
+    A, Λ 
 end
 
-function init_output(client_matrix, station_list)
-    client_count = length(client_matrix[client_matrix .> 0])
-    station_count = size(station_list, 1)
-    A = zeros(client_count, station_count)
-    idx = non_zero_list(client_matrix)
-    A, idx
+function make_probability(i, j, d_csr, d_client, p_dist) 
+    d_csr /= log(p_dist)
+    d_client /= log(p_dist)
+    p_csr = (x, y) -> exp(-dist(i, j, x, y) / d_csr)
+    p_client = (x, y) -> exp(-dist(i, j, x, y) / d_client)
+    p_csr, p_client
 end
 
-function non_zero_list(M)
-    x, y = size(M)
-    [(i, j, M[i, j]) for i ∈ 1:x for j ∈ 1:y if M[i, j] > 0]
-end
 
+function init_alpha_matrix(cm, sm) 
+    x = size(cm, 1)
+    y = size(sm, 1)
+    zeros(y, x), zeros(y)
+end
 
 dist = (x₁, y₁, x₂, y₂) -> √((x₁ - x₂)^2 + (y₁ - y₂)^2)
-
